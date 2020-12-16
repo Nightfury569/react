9bc94f7c972f99a1411a865bec6d74ea0aaf70c3
Nightfury569 <pnightfury7@gmail.com>
2020-12-17 00:54:42 +0530

    Updates

---
+++ precache-manifest.e209f55562c81fcc47e46430ccd522ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e19ffb83b5eacff7be58285b0fa884",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d98a43125acaff559bf4",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ef76c0fb45fbb95daa9",
+    "url": "/react/static/js/2.c711bf51.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react/static/js/2.c711bf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98a43125acaff559bf4",
+    "url": "/react/static/js/main.c1e43990.chunk.js"
+  },
+  {
+    "revision": "3003b2dc6178e0d83552",
+    "url": "/react/static/js/runtime-main.2d789757.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
